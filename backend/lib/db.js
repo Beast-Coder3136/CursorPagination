0ceb9c958b314ac42ec818e2996040aeb8cdd0d0
Anas <mohdanasali3136@gamil.com>
2026-06-23 23:44:20 +0530

    initial commit

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose" 
 import dotenv from 'dotenv'
 dotenv.config({ });
-console.log(process.env.MONGO_URL)
 export const connectDB = async()=>{
   try {
     const res = await mongoose.connect(process.env.MONGO_URL)
